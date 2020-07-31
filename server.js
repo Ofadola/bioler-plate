ec497de89a97624d50760e8b7eb0d310e4c6d043
ofadola <busayofadola.com>
2020-07-31 15:53:50 +1000

    i commited these file

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-var express = require('express')
+var express = require('express');
+const { request } = require('express');
 var app = express()
 
 app.use(express.static(__dirname + '/public'));
+
+var adder=function(num1,num2){
+    var result = num1+num2;
+    return result
+
+}
+
+app.get('/test',function(req,res){
+    var username=req.query.username;
+    console.log('ye i have been hit'+username);
+    res.send('Hello you hit me!  '+username);
+})
+
+app.get('/adder', function(req,res){
+    var num1=parseInt(req.query.num1);
+    var num2=parseInt(req.query.num2);
+    var result=adder(num1,num2)
+    res.send('The result is: '+result)
+})
 
 var port=3000;
 app.listen(port)
